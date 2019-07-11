2878301541b82fd9bdf62ac6f29af9021979bb25
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2019-07-11 12:01:37 -0400

    Running GAM (quasipoisson) and BART using latest data

--- 01-country-analysis.R
+++ 01-country-analysis.R
@@ -22,9 +22,9 @@ set.seed(101)
 
 # prep data
 country_dat <- read_csv(here::here("country_level_amr.csv")) %>%
-  #select(-livestock_ab_sales_kg) %>% # temp remove until NA handling is implemented
+  select(-livestock_ab_sales_kg) %>% # temp remove until NA handling is implemented
   select(-continent, -region) %>%
-  #na.omit() %>%
+  na.omit() %>%
   mutate(gdp_dollars = log10(gdp_dollars/1000000000),
          population = log10(population)) %>%
   rename(gdp_billion_log = gdp_dollars,
@@ -49,96 +49,33 @@ chart.Correlation(country_dat %>%
                   histogram=FALSE, pch=19)
 
 #' -----------------Fit GAM-----------------
-#' using ziplss family- The ziplss family implements a zero inflated Poisson model in which one linear predictor controls the probability of presence and the other controls the mean given presence. 
 #+ r mod-gam
 # sp = smoothing parameter (higher = smoother, can be fit with method = "REML")
 # k  = number of base curves
 
-# handling NAs
-## process data.frame producing binary indicators of missingness,
-## mx0, mx1 etc. For each missing value create a level of a factor
-## idx0, idx1, etc. So idx0 has as many levels as x0 has missing 
-## values. Replace the NA's in each variable by the mean of the 
-## non missing for that variable...
+gam_mod <- gam(data = country_dat, 
+               formula = n_amr_events ~  
+                 s(gdp_billion_log, k = 4) + 
+                 s(population_log, k = 10) +
+                 s(migrant_pop_perc, k = 3) +
+                 s(health_expend_perc, k = 10) +
+                 s(ab_export_perc, k = 3) +
+                 s(pubs_sum, k = 10) +
+                 s(ab_consumption_ddd, k = 10) +
+                 s(ag_land_perc, k = 10) +
+                 s(manure_soils_kg_per_km2, k = 3) +
+                 english_spoken,
+               method = "REML",
+               family = "quasipoisson")
 
-dname <- country_dat %>%
-  select_if(~any(is.na(.))) %>%
-  select(-english_spoken) %>%
-  colnames()
-dat1 <- country_dat
-for (i in 1:length(dname)) {
-  n <- nrow(country_dat)
-  by.name <- paste("m",dname[i],sep="")
-  dat1[[by.name]] <- is.na(dat1[[dname[i]]])
-  dat1[[dname[i]]][dat1[[by.name]]] <- mean(dat1[[dname[i]]],na.rm=TRUE)
-  lev <- rep(1,n);lev[dat1[[by.name]]] <- 1:sum(dat1[[by.name]])
-  id.name <- paste("id",dname[i],sep="")
-  dat1[[id.name]] <- factor(lev)
-  dat1[[by.name]] <- as.numeric(dat1[[by.name]])
-}
-
-## Fit a gam, in which any missing value contributes zero 
-## to the linear predictor from its smooth, but each 
-## missing has its own random effect, with the random effect 
-## variances being specific to the variable. e.g.
-## for s(x0,by=ordered(!mx0)), declaring the `by' as an ordered
-## factor ensures that the smooth is centred, but multiplied
-## by zero when mx0 is one (indicating a missing x0). This means
-## that any value (within range) can be put in place of the 
-## NA for x0.  s(idx0,bs="re",by=mx0) produces a separate Gaussian 
-## random effect for each missing value of x0 (in place of s(x0),
-## effectively). The `by' variable simply sets the random effect to 
-## zero when x0 is non-missing, so that we can set idx0 to any 
-## existing level for these cases.   
-k_tune <- 5
-gam_mod <- gam(data = dat1, 
-               formula = list(
-                 # first specifies the response on the left hand side and the structure of the linear predictor for the Poisson parameter on the right hand side
-                 n_amr_events ~ 
-                   s(pubs_sum, by = ordered(!mpubs_sum), k = k_tune) + 
-                   s(idpubs_sum, bs="re", by=mpubs_sum, k = k_tune) +
-                   s(gdp_billion_log, by = ordered(!mgdp_billion_log), k = k_tune) + 
-                   s(idgdp_billion_log, bs="re", by=mgdp_billion_log, k = k_tune) +
-                   s(population_log, by = ordered(!mpopulation_log), k = k_tune) + 
-                   s(idpopulation_log, bs="re", by=mpopulation_log, k = k_tune) +
-                   english_spoken,
-                 # specifying the linear predictor for the probability of presence on the right hand side
-                 ~  
-                   s(ab_consumption_ddd, by = ordered(!mab_consumption_ddd), k = k_tune) +
-                   s(idab_consumption_ddd, bs="re", by=mab_consumption_ddd, k = k_tune) +
-                   s(health_expend_perc, by = ordered(!mhealth_expend_perc), k = k_tune) + 
-                   s(idhealth_expend_perc, bs="re", by=mhealth_expend_perc, k = k_tune) #+
-                   # s(livestock_ab_sales_kg, by = ordered(!mlivestock_ab_sales_kg), k = k_tune) +
-                   # s(idlivestock_ab_sales_kg, bs="re", by=mlivestock_ab_sales_kg, k = k_tune)
-                   # s(ag_land_perc, by = ordered(!mag_land_perc), k = k_tune) +
-                   # s(idag_land_perc, bs="re", by=mag_land_perc, k = k_tune)
-                   # s(ab_export_perc, by = ordered(!mab_export_perc), k = k_tune) +
-                   # s(idab_export_perc, bs="re", by=mab_export_perc, k = k_tune) +
-                   # s(manure_soils_kg_per_km2) +
-                   # s(migrant_pop_perc) +
-               ),
-               method = "REML", # to set sp
-               family = ziplss())
-# Notes for next steps
-# brms - neg binomial - or hurdle - brm(formula, data), bf(count ~ ..., theta, formula) stick this into brm( with family = hurdle.poisson) - 4 chains on egypti
-# bayes plot, sjplot::plotmodel() - missing data in brms - mice multiple imputation and then fit - haas option of fitting missing data.
-
-gam_mod$outer.info$conv
 summary(gam_mod) # higher EDF = more wiggly (1 = linear)
-#concurvity(gam_mod, full = TRUE) # Concurvity occurs when some smooth term in a model could be approximated by one or more of the other smooth terms in the model. 
+concurvity(gam_mod, full = TRUE) # Concurvity occurs when some smooth term in a model could be approximated by one or more of the other smooth terms in the model. 
 gam.check(gam_mod)
 
 plot(gam_mod,
      all.terms = FALSE,
      residuals = TRUE, pch = 1, cex = 1, shade = TRUE, shade.col = "lightblue",
      seWithMean = TRUE, shift = coef(gam_mod)[1])
-
-## more checking...
-## 1. If the zero inflation rate becomes decoupled from the linear predictor, 
-## it is possible for the linear predictor to be almost unbounded in regions
-## containing many zeroes. So examine if the range of predicted values 
-## is sane for the zero cases? 
-range(predict(gam_mod,type="response")[gam_mod$y==0]) # range is reasonable
 
 #' -----------------Fit BART-----------------
 #+ r mod-bart
@@ -151,52 +88,52 @@ bart_mod <- bart(country_dat %>% dplyr::select(-n_amr_events, -iso3c, -country),
 #+ r mod-comp
 
 # model results
-# country_dat %<>% mutate(id = row_number())
-# gt <- tibble(gam = predict(gam_mod, type="response"), id = country_dat %>% filter(!is.na(pubs_sum), !is.na(gdp_billion_log)) %>% pull(id))
-# bt <- tibble(bart = bart_mod$yhat.train.mean, id = country_dat %>% na.omit() %>% pull(id))
-# 
-# # add in model predictions
-# country_dat2 <- country_dat %>%
-#   left_join(gt) %>%
-#   left_join(bt) %>%
-#   dplyr::select(-country, -iso3c, -id) 
-# 
-# # reshape
-# country_dat_rs2 <- country_dat2  %>%
-#   gather(key = "var", value = "x", -n_amr_events, -gam, -bart) %>%
-#   gather(key = "model", value = "predicted", -x, -n_amr_events, -var) %>%
-#   mutate(residual = n_amr_events - predicted,
-#          var = factor(var, levels = c("pubs_sum",
-#                                       "gdp_billion_log",
-#                                       "population_log",
-#                                       "migrant_pop_perc",
-#                                       "health_expend_perc",
-#                                       "ab_export_perc",
-#                                       "ab_consumption_ddd",
-#                                       "ag_land_perc",
-#                                       #"manure_soils_kg_per_km2",
-#                                       "english_spoken"),
-#                       labels = c("Pubcrawler sum",
-#                                  "GDP ($ billion; log scale)",
-#                                  "Population (log scale)",
-#                                  "Migrant Population (% total population)",
-#                                  "Health Expenditure (% GDP)",
-#                                  "Antibiotic Exports ($ billion; log scale)",
-#                                  "Antibiotic Consumption (DDD per capita)",
-#                                  "Agricultural Land Coverage (%)",
-#                                  #"Manure applied to soils (N content; kg)",
-#                                  "English Spoken"))) 
-# 
-# # mean residuals
-# country_dat_rs2 %>%
-#   group_by(model) %>%
-#   summarize(mean_residuals = mean(abs(residual), na.rm=TRUE))
-# 
-# # covariance of predictions with n
-# country_dat2 %>%
-#   dplyr::select(n_amr_events, gam, bart) %>%
-#   as.matrix() %>%
-#   cor() 
+country_dat %<>% mutate(id = row_number())
+gt <- tibble(gam = predict(gam_mod, type="response"), id = country_dat %>% filter(!is.na(pubs_sum), !is.na(gdp_billion_log)) %>% pull(id))
+bt <- tibble(bart = bart_mod$yhat.train.mean, id = country_dat %>% na.omit() %>% pull(id))
+
+# add in model predictions
+country_dat2 <- country_dat %>%
+  left_join(gt) %>%
+  left_join(bt) %>%
+  dplyr::select(-country, -iso3c, -id) 
+
+# reshape
+country_dat_rs2 <- country_dat2  %>%
+  gather(key = "var", value = "x", -n_amr_events, -gam, -bart) %>%
+  gather(key = "model", value = "predicted", -x, -n_amr_events, -var) %>%
+  mutate(residual = n_amr_events - predicted,
+         var = factor(var, levels = c("pubs_sum",
+                                      "gdp_billion_log",
+                                      "population_log",
+                                      "migrant_pop_perc",
+                                      "health_expend_perc",
+                                      "ab_export_perc",
+                                      "ab_consumption_ddd",
+                                      "ag_land_perc",
+                                      #"manure_soils_kg_per_km2",
+                                      "english_spoken"),
+                      labels = c("Pubcrawler sum",
+                                 "GDP ($ billion; log scale)",
+                                 "Population (log scale)",
+                                 "Migrant Population (% total population)",
+                                 "Health Expenditure (% GDP)",
+                                 "Antibiotic Exports ($ billion; log scale)",
+                                 "Antibiotic Consumption (DDD per capita)",
+                                 "Agricultural Land Coverage (%)",
+                                 #"Manure applied to soils (N content; kg)",
+                                 "English Spoken"))) 
+
+# mean residuals
+country_dat_rs2 %>%
+  group_by(model) %>%
+  summarize(mean_residuals = mean(abs(residual), na.rm=TRUE))
+
+# covariance of predictions with n
+country_dat2 %>%
+  dplyr::select(n_amr_events, gam, bart) %>%
+  as.matrix() %>%
+  cor() 
 
 #' -----------------Residuals-----------------
 #+ r resids
@@ -217,8 +154,7 @@ bart_mod <- bart(country_dat %>% dplyr::select(-n_amr_events, -iso3c, -country),
 
 # ICE plots
 mod_dat <-  country_dat %>%
-  select(-iso3c, -country, #-id, 
-         -n_amr_events) %>%
+  select(-iso3c, -country, -id, -n_amr_events) %>%
   as.data.frame()
 
 # function for bart and gam model predict
--- 01-country-analysis.md
+++ 01-country-analysis.md
@@ -1,93 +1,89 @@
 exploratory\_analysis
 ================
 emmamendelsohn
-2019-06-25
+2019-07-11
 
 —————–View Data—————–
 
 ![](01-country-analysis_files/figure-gfm/r%20plots-1.png)<!-- -->![](01-country-analysis_files/figure-gfm/r%20plots-2.png)<!-- -->
 
-—————–Fit GAM—————– using ziP family- for zero inflated Poisson data,
-when the zero inflation rate depends simply on the Poisson mean. From
-mgcv: This sort of model is really only appropriate when none of your
-covariates help to explain the zeroes in your data. If your covariates
-predict which observations are likely to have zero mean then adding a
-zero inflated model on top of this is likely to lead to identifiability
-problems. Identifiability problems may lead to fit failures, or absurd
-values for the linear predictor or predicted values.
-
-    ## [1] "full convergence"
+—————–Fit GAM—————–
 
     ## 
-    ## Family: Zero inflated Poisson(-1.016,0.615) 
-    ## Link function: identity 
+    ## Family: quasipoisson 
+    ## Link function: log 
     ## 
     ## Formula:
-    ## n_amr_events ~ s(gdp_billion_log) + s(population_log) + s(pubs_sum) + 
-    ##     s(migrant_pop_perc, k = 5) + s(health_expend_perc) + s(ab_export_perc, 
-    ##     k = 5) + s(ab_consumption_ddd) + s(ag_land_perc, k = 5) + 
-    ##     s(manure_soils_kg_per_km2, k = 5) + english_spoken
+    ## n_amr_events ~ s(gdp_billion_log, k = 4) + s(population_log, 
+    ##     k = 10) + s(migrant_pop_perc, k = 3) + s(health_expend_perc, 
+    ##     k = 10) + s(ab_export_perc, k = 3) + s(pubs_sum, k = 10) + 
+    ##     s(ab_consumption_ddd, k = 10) + s(ag_land_perc, k = 10) + 
+    ##     s(manure_soils_kg_per_km2, k = 3) + english_spoken
     ## 
     ## Parametric coefficients:
-    ##                    Estimate Std. Error z value Pr(>|z|)    
-    ## (Intercept)         2.41457    0.07606  31.746   <2e-16 ***
-    ## english_spokenTRUE  0.12162    0.16610   0.732    0.464    
+    ##                    Estimate Std. Error t value Pr(>|t|)    
+    ## (Intercept)          2.2520     0.1588  14.182   <2e-16 ***
+    ## english_spokenTRUE   0.3698     0.2581   1.433    0.159    
     ## ---
     ## Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
     ## 
     ## Approximate significance of smooth terms:
-    ##                              edf Ref.df Chi.sq  p-value    
-    ## s(gdp_billion_log)         1.000  1.000  6.005   0.0143 *  
-    ## s(population_log)          6.998  7.907 86.316 2.82e-15 ***
-    ## s(pubs_sum)                5.437  6.100 28.194 9.49e-05 ***
-    ## s(migrant_pop_perc)        1.000  1.000  0.081   0.7765    
-    ## s(health_expend_perc)      6.972  7.633 81.913 1.10e-14 ***
-    ## s(ab_export_perc)          1.000  1.000  1.116   0.2907    
-    ## s(ab_consumption_ddd)      2.219  2.717 31.400 2.38e-05 ***
-    ## s(ag_land_perc)            3.813  3.951 35.367 2.09e-07 ***
-    ## s(manure_soils_kg_per_km2) 3.885  3.969 39.231 5.70e-08 ***
+    ##                              edf Ref.df      F  p-value    
+    ## s(gdp_billion_log)         1.000  1.000  0.401 0.529662    
+    ## s(population_log)          2.515  3.069  5.965 0.001465 ** 
+    ## s(migrant_pop_perc)        1.000  1.000  0.902 0.347281    
+    ## s(health_expend_perc)      2.405  3.041  4.926 0.004579 ** 
+    ## s(ab_export_perc)          1.000  1.000  2.074 0.156660    
+    ## s(pubs_sum)                1.000  1.000  1.433 0.237416    
+    ## s(ab_consumption_ddd)      1.784  2.232 10.026 0.000166 ***
+    ## s(ag_land_perc)            3.349  4.088  4.071 0.006180 ** 
+    ## s(manure_soils_kg_per_km2) 1.000  1.000  0.243 0.624356    
     ## ---
     ## Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
     ## 
-    ## Deviance explained = 99.3%
-    ## -REML = 229.43  Scale est. = 1         n = 63
+    ## R-sq.(adj) =  0.862   Deviance explained = 81.3%
+    ## -REML = 112.25  Scale est. = 8.2384    n = 62
 
-    ##               para s(gdp_billion_log) s(population_log) s(pubs_sum)
-    ## worst    0.9999543          1.0000000         0.9999999   1.0000000
-    ## observed 0.9999543          0.9895073         0.9669500   0.9948870
-    ## estimate 0.9999543          0.9865408         0.9791193   0.9990749
+    ##               para s(gdp_billion_log) s(population_log)
+    ## worst    0.8749652          0.9913633         0.9927653
+    ## observed 0.8749652          0.9839572         0.9695050
+    ## estimate 0.8749652          0.9861254         0.9347583
     ##          s(migrant_pop_perc) s(health_expend_perc) s(ab_export_perc)
-    ## worst              0.9999996             1.0000000         0.9999997
-    ## observed           0.9537991             0.9658247         0.9415446
-    ## estimate           0.9382690             0.9645454         0.9440537
-    ##          s(ab_consumption_ddd) s(ag_land_perc) s(manure_soils_kg_per_km2)
-    ## worst                0.9999994       0.9999944                  0.9999986
-    ## observed             0.8708048       0.9168492                  0.9278401
-    ## estimate             0.8944577       0.9834974                  0.9664509
+    ## worst              0.9102837             0.9999750         0.8960028
+    ## observed           0.8979236             0.9416345         0.8951820
+    ## estimate           0.8958291             0.9360546         0.8931218
+    ##          s(pubs_sum) s(ab_consumption_ddd) s(ag_land_perc)
+    ## worst      0.9999757             0.9734763       0.9646690
+    ## observed   0.9933393             0.7914903       0.7143527
+    ## estimate   0.9965503             0.7441085       0.7103518
+    ##          s(manure_soils_kg_per_km2)
+    ## worst                     0.9397263
+    ## observed                  0.8440649
+    ## estimate                  0.8461450
 
 ![](01-country-analysis_files/figure-gfm/r%20mod-gam-1.png)<!-- -->
 
     ## 
     ## Method: REML   Optimizer: outer newton
-    ## full convergence after 16 iterations.
-    ## Gradient range [-7.595193e-05,2.043814e-05]
-    ## (score 229.4294 & scale 1).
-    ## Hessian positive definite, eigenvalue range [1.598647e-06,73.585].
-    ## Model rank =  63 / 63 
+    ## full convergence after 11 iterations.
+    ## Gradient range [-1.962972e-05,1.998476e-05]
+    ## (score 112.2496 & scale 8.238359).
+    ## Hessian positive definite, eigenvalue range [4.585919e-07,25.59512].
+    ## Model rank =  56 / 56 
     ## 
     ## Basis dimension (k) checking results. Low p-value (k-index<1) may
     ## indicate that k is too low, especially if edf is close to k'.
     ## 
-    ##                              k'  edf k-index p-value   
-    ## s(gdp_billion_log)         9.00 1.00    0.72   0.030 * 
-    ## s(population_log)          9.00 7.00    1.14   0.930   
-    ## s(pubs_sum)                9.00 5.44    0.65   0.005 **
-    ## s(migrant_pop_perc)        4.00 1.00    1.09   0.860   
-    ## s(health_expend_perc)      9.00 6.97    0.86   0.215   
-    ## s(ab_export_perc)          4.00 1.00    1.06   0.800   
-    ## s(ab_consumption_ddd)      9.00 2.22    1.22   1.000   
-    ## s(ag_land_perc)            4.00 3.81    0.80   0.095 . 
-    ## s(manure_soils_kg_per_km2) 4.00 3.89    0.88   0.255   
+    ##                              k'  edf k-index p-value  
+    ## s(gdp_billion_log)         3.00 1.00    0.82    0.09 .
+    ## s(population_log)          9.00 2.52    0.91    0.30  
+    ## s(migrant_pop_perc)        2.00 1.00    1.15    0.94  
+    ## s(health_expend_perc)      9.00 2.40    1.00    0.56  
+    ## s(ab_export_perc)          2.00 1.00    0.83    0.12  
+    ## s(pubs_sum)                9.00 1.00    0.82    0.10  
+    ## s(ab_consumption_ddd)      9.00 1.78    1.14    0.95  
+    ## s(ag_land_perc)            9.00 3.35    0.93    0.33  
+    ## s(manure_soils_kg_per_km2) 2.00 1.00    1.07    0.81  
     ## ---
     ## Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
 
@@ -104,14 +100,14 @@ values for the linear predictor or predicted values.
     ##  k: 2.000000
     ##  degrees of freedom in sigma prior: 3.000000
     ##  quantile in sigma prior: 0.900000
-    ##  scale in sigma prior: 0.005576
+    ##  scale in sigma prior: 0.005321
     ##  power and base for tree prior: 2.000000 0.950000
     ##  use quantiles for rule cut points: false
     ## data:
-    ##  number of training observations: 63
+    ##  number of training observations: 62
     ##  number of test observations: 0
     ##  number of explanatory variables: 10
-    ##  init sigma: 25.039913, curr sigma: 25.039913
+    ##  init sigma: 20.495039, curr sigma: 20.495039
     ## 
     ## Cutoff rules c in x<=c vs x>c
     ## Number of cutoffs: (var: number of possible c):
@@ -129,28 +125,39 @@ values for the linear predictor or predicted values.
     ## iteration: 800 (of 1000)
     ## iteration: 900 (of 1000)
     ## iteration: 1000 (of 1000)
-    ## total seconds in loop: 0.893443
+    ## total seconds in loop: 1.148766
     ## 
     ## Tree sizes, last iteration:
-    ## [1] 2 2 2 2 2 2 2 1 2 3 2 2 2 3 2 3 2 2 
-    ## 2 2 4 2 2 3 2 1 3 2 2 2 2 3 2 2 2 2 3 2 
-    ## 2 2 2 2 3 3 2 2 3 2 2 3 2 2 3 2 2 2 3 2 
-    ## 2 2 3 2 2 2 3 2 2 3 2 2 2 2 3 3 2 3 2 2 
-    ## 4 2 2 5 3 2 2 2 2 3 2 2 2 3 2 2 2 2 2 2 
-    ## 2 2 3 4 2 2 2 1 3 2 2 2 2 2 2 2 3 2 4 4 
-    ## 1 2 2 2 2 2 2 2 2 2 2 2 3 3 2 1 2 2 3 2 
-    ## 2 3 2 3 2 4 2 2 3 3 2 2 2 2 2 2 2 2 3 2 
-    ## 3 3 2 2 2 2 2 2 2 2 1 2 2 2 2 2 2 2 3 3 
-    ## 2 2 2 4 2 2 2 2 2 2 2 2 2 2 3 3 2 2 2 2 
-    ## 2 1 
+    ## [1] 2 2 3 2 2 2 2 4 3 3 2 2 2 3 2 3 3 5 
+    ## 1 2 3 2 2 2 2 3 2 3 2 2 2 3 2 2 3 3 2 2 
+    ## 2 2 2 3 2 2 2 2 1 2 3 2 2 2 4 2 2 3 5 2 
+    ## 4 3 2 2 3 2 2 2 2 2 2 2 2 2 3 3 2 2 2 2 
+    ## 3 2 2 2 2 2 2 3 2 2 2 2 2 2 2 2 2 2 3 2 
+    ## 2 2 3 2 2 2 2 2 3 2 2 1 1 2 2 3 2 3 3 2 
+    ## 3 3 3 3 3 3 2 2 2 1 2 2 2 2 2 3 2 2 3 2 
+    ## 2 2 3 1 2 2 2 3 3 3 2 4 2 2 3 3 2 2 2 2 
+    ## 3 1 3 3 2 3 3 2 2 3 3 2 2 2 2 2 2 3 2 3 
+    ## 2 3 2 1 2 2 2 3 2 2 3 2 2 2 1 3 3 2 3 3 
+    ## 2 3 
     ## 
     ## Variable Usage, last iteration (var:count):
-    ## (1: 24) (2: 27) (3: 26) (4: 30) (5: 18) 
-    ## (6: 30) (7: 28) (8: 20) (9: 22) (10: 24) 
+    ## (1: 23) (2: 30) (3: 33) (4: 25) (5: 25) 
+    ## (6: 28) (7: 27) (8: 29) (9: 24) (10: 19) 
     ## 
     ## DONE BART
 
 —————–Compare models—————–
+
+    ## # A tibble: 2 x 2
+    ##   model mean_residuals
+    ##   <chr>          <dbl>
+    ## 1 bart            9.61
+    ## 2 gam             6.23
+
+    ##              n_amr_events       gam      bart
+    ## n_amr_events    1.0000000 0.9485646 0.9183702
+    ## gam             0.9485646 1.0000000 0.9625171
+    ## bart            0.9183702 0.9625171 1.0000000
 
 —————–Residuals—————–
 
