ff8ea6280720be7e4757fff9c51649d3d07154ea
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2023-03-28 13:44:09 -0400

    fix typo in label, update renv

--- R/plot_labels.R
+++ R/plot_labels.R
@@ -5,27 +5,27 @@ lookup_vars <- c(
   # surveillance 
   "ln_population" = "ln(Population)",
   "english_spoken" = "English Spoken (yes/no)", 
-  "ln_pubcrawl_per_capita" = "ln(Publication Bias Index [per capita])",
-  "ln_promed_mentions_per_capita" = "ln(ProMed Mentions [per capita])",
+  "ln_pubcrawl_per_capita" = "ln(Publication Bias Index [per cap.])",
+  "ln_promed_mentions_per_capita" = "ln(ProMed Mentions [per cap.])",
   
   # gdp
-  "ln_gdp_per_capita" = "ln(GDP [dollars per capita])", 
+  "ln_gdp_per_capita" = "ln(GDP [dollars per cap.])", 
   "health_expend_perc" = "Health Expenditure (% GDP)", 
   
   # consumption vars
   "human_consumption_ddd" = "Human AB Consumption (DDD)", 
-  "ln_livestock_consumption_kg_per_capita" = "Livestock AB Consumption [kg per capita)",
+  "ln_livestock_consumption_kg_per_capita" = "Livestock AB Consumption (kg per cap.)",
   "ln_livestock_consumption_kg_per_capita.ln_gdp_per_capita"= "Livestock AB Consumption:GDP",
   "ln_livestock_consumption_kg_per_capita:ln_gdp_per_capita"= "Livestock AB Consumption:GDP",
   "ln_livestock_consumption_kg_per_pcu" = "ln(Livestock AB Consumption [per PCU])",
   "ln_livestock_pcu" = "ln(Livestock Population [PCU])", 
   
   # production
-  "ln_ab_export_per_capita" = "ln(AB Exports [dollars per capita])", 
+  "ln_ab_export_per_capita" = "ln(AB Exports [dollars per cap.])", 
   "ab_export_bin" = "AB Exported (yes/no)",
   
   # population movement
-  "ln_tourism_outbound_per_capita" = "ln(Tourism - Outbound [per capita])", 
-  "ln_tourism_inbound_per_capita"  = "ln(Tourism - Inbound [per capita])",
-  "ln_migrant_pop_per_capita" = "ln(Migrant Population [per capita])" 
+  "ln_tourism_outbound_per_capita" = "ln(Tourism - Outbound [per cap.])", 
+  "ln_tourism_inbound_per_capita"  = "ln(Tourism - Inbound [per cap.])",
+  "ln_migrant_pop_per_capita" = "ln(Migrant Population [per cap.])" 
 )
